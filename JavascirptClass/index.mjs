e5fc77eb11dab3aa47d973c7b75a96e32bd00764
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2022-03-23 10:56:55 +0000

    Still learning JS

--- JavascirptClass/index.mjs
+++ JavascirptClass/index.mjs
@@ -1,6 +1,64 @@
-let arr = [99, 45, 75];
-let iterator = arr[Symbol.iterator]();
-for (let result = iterator.next(); !result.done; result = iterator.next()) {
-  console.log(result.value);
+import { createConnection } from "mysql";
+import fs from "fs";
+
+let connection = createConnection({
+  host: "localhost",
+  user: "JavaScript",
+  password: "akwasi0556446613",
+  database: "JavaScriptDB",
+});
+
+function connectToDB() {
+  connection.connect((err) => {
+    if (err) {
+      console.error(`Couldn't connect to DB, error: ${err}`);
+    } else console.log(`Connected successfully`);
+  });
 }
-``
+
+function insertIntoDB() {
+  connection.query(
+    "INSERT INTO EMPLOYEE (Name, Sex) VALUES ('Vivian Akuoko', 'F'), ('Yaw Akuoko', 'M')",
+    (err, results) => {
+      if (err) {
+        console.error(`Couldn't insert values into DB, error: ${err}`);
+      } else {
+        console.log(`Values inserted successfully, results: ${results}`);
+      }
+    }
+  );
+}
+
+function queryResultsAndStore() {
+  connection.query("SELECT * FROM EMPLOYEE", (err, results) => {
+    if (err) {
+      console.error(`Couldn't query results, error: ${err}`);
+    } else {
+      let employees = JSON.stringify(results);
+      employees = JSON.parse(employees);
+      for (const keys in employees) {
+        console.log(keys, employees[keys]);
+      }
+      employees = JSON.stringify(employees, null, 2);
+      fs.writeFile("employees-1.json", employees, (err) => {
+        if (err) throw err;
+        else console.log("File written successfully");
+      });
+    }
+  });
+}
+
+function closeDB() {
+  connection.end((err) => {
+    if (err) {
+      console.error(`Connection not closed, error: ${err}`);
+    } else {
+      console.log(`DB disconnected successfully`);
+    }
+  });
+}
+
+connectToDB();
+// insertIntoDB();
+queryResultsAndStore();
+closeDB();
