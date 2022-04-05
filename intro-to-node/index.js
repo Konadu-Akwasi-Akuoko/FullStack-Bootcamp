8e020c45a9320773981e333d5a9c94c5bc836c61
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2022-04-05 04:20:24 +0000

    Finished intro to node

---
+++ intro-to-node/index.js
@@ -0,0 +1,6 @@
+const superheroes = require("superheroes");
+const supervillains = require("supervillains");
+
+console.log(
+  `My superhero name is ${superheroes.random()} and my supervillain name is ${supervillains.random()}`
+);
