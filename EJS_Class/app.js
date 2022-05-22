75c0602b1a4f49ee3c626408f621eb1e52f09288
Konadu Akwasi Akuoko <konaduakwasiakuoko@gmail.com>
2022-05-22 20:02:24 +0000

    Created a one time dynamic ToDo list with 2 ejs files

--- EJS_Class/app.js
+++ EJS_Class/app.js
@@ -11,15 +11,23 @@ app.use(express.json());
 app.set("view engine", "ejs");
 const port = process.env.PORT || 3000;
 
+let date = new Date();
+const options = {
+  weekday: "long",
+  month: "long",
+  day: "numeric",
+};
+
 app.get("/", (req, res) => {
-  let today = new Date();
-  let day = "";
-  if (today.getDay() === 6 || today.getDay() === 0) {
-    day = `${today.getDate()}`;
-  } else {
-    day = `${today.getDate()}`;
-  }
-  res.render("list", { day: day });
+  res.render("list", { _date: date.toLocaleDateString("default", options) });
+});
+
+app.post("/", (req, res) => {
+  console.log("Post request received " + req.body.toDoItem);
+  res.render("list2", {
+    _date: date.toLocaleDateString("default", options),
+    addToDo1: req.body.toDoItem,
+  });
 });
 
 app.listen(port, () => {
