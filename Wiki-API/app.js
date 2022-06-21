c3a698d103f4984433330a0badcaebfd5c2b58ab
Konadu Akwasi Akuoko <konaduakwasiakuoko@gmail.com>
2022-06-21 04:48:26 +0000

    Added a put, delete and post route to /articles

--- Wiki-API/app.js
+++ Wiki-API/app.js
@@ -16,11 +16,61 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.get("/articles", (req, res) => {
-  db.getAllArticles().then((data) => {
-    res.send(data);
+app
+  .route("/articles")
+
+  .get((req, res) => {
+    db.getAllArticles()
+      .then((data) => {
+        res.send(data);
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  })
+
+  .post((req, res) => {
+    db.addArticle(req.body.title, req.body.content).then((data) => {
+      res.send({ success: true, ...data._doc });
+    });
+  })
+
+  .delete((req, res) => {
+    db.deleteAll()
+      .then((data) => {
+        res.send(data);
+      })
+      .catch((err) => {
+        res.send(err);
+      });
   });
-});
+
+app
+  .route("/articles/:title")
+
+  .get((req, res) => {
+    db.getAnArticle(req.params.title)
+      .then((data) => {
+        res.send(data);
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  })
+
+  .put((req, res) => {
+    db.updateAWholeArticle(req.params.title, req.body.title, req.body.content)
+      .then((data) => {
+        if (data.acknowledged === true) {
+          res.send({ success: true, ...data });
+        } else {
+          res.send({ success: false, ...data });
+        }
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  });
 
 app.listen(port, () => {
   console.log(`Sever is running on port ${port}!`);
--- Wiki-API/db.js
+++ Wiki-API/db.js
@@ -5,6 +5,7 @@ mongoose.connect("mongodb://localhost:27017/wikiDB", (err, data) => {
     console.log("Error connecting to MongoDB");
   } else {
     console.log("Connected to MongoDB");
+    //console.log(data);
   }
 });
 
@@ -27,6 +28,75 @@ let db = {
       Article.find({}, (err, data) => {
         if (err) {
           console.log("Error getting all articles");
+          console.log(err);
+          reject(err);
+        } else {
+          //console.log(data);
+          resolve(data);
+        }
+      });
+    });
+  },
+
+  getAnArticle: (title) => {
+    return new Promise((resolve, reject) => {
+      Article.find({ title: title }, (err, data) => {
+        if (err) {
+          console.log("Error getting all articles");
+          console.log(err);
+          reject(err);
+        } else {
+          //console.log(data);
+          resolve(data);
+        }
+      });
+    });
+  },
+
+  addArticle: (title, content) => {
+    return new Promise((resolve, reject) => {
+      let article = new Article({
+        title: title,
+        content: content,
+      });
+      article.save((err, data) => {
+        if (err) {
+          console.log("Error adding article");
+          console.log(err);
+          reject(err);
+        } else {
+          //console.log(data);
+          resolve(data);
+        }
+      });
+    });
+  },
+
+  updateAWholeArticle: (titleToUpdate, title, content) => {
+    return new Promise((resolve, reject) => {
+      Article.replaceOne(
+        { title: titleToUpdate },
+        { title: title, content: content },
+        (err, data) => {
+          if (err) {
+            console.log("Error updating article");
+            console.log(err);
+            reject(err);
+          } else {
+            //console.log(data);
+            resolve(data);
+          }
+        }
+      );
+    });
+  },
+
+  deleteAll: () => {
+    return new Promise((resolve, reject) => {
+      Article.deleteMany({}, (err, data) => {
+        if (err) {
+          console.log("Error deleting all articles");
+          console.log(err);
           reject(err);
         } else {
           //console.log(data);
