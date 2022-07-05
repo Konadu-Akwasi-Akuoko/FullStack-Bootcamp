003c039405e6496598ead1c97b198c0f3afb255c
Konadu Akwasi Akuoko <konaduakwasiakuoko@gmail.com>
2022-07-05 11:20:07 +0000

    Done with chapter 2 of Programming Typescript

---
+++ typescript/Chapter02/dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+let a = 1 + 2;
+let b = a + 3 + "Who are you";
+let c = {
+    apple: a,
+    banana: b,
+};
+let d = c.apple * 4 + "I am a boy";
+console.log(d);
+//# sourceMappingURL=index.js.map
