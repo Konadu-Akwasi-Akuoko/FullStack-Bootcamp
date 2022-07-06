abb0d965c7f4a7ed864d10120b7e1df95e055624
Konadu Akwasi Akuoko <konaduakwasiakuoko@gmail.com>
2022-07-06 09:07:52 +0000

    Done with the basics of TypeScript

--- typescript/Chapter02/dist/index.js
+++ typescript/Chapter02/dist/index.js
@@ -1,10 +1,32 @@
 "use strict";
-let a = 1 + 2;
-let b = a + 3 + "Who are you";
-let c = {
-    apple: a,
-    banana: b,
+Object.defineProperty(exports, "__esModule", { value: true });
+let a = {
+    a: "I am a boy",
+    b: 24,
+    c: () => `${a.b} and ${a.a}`,
+    23: 432,
+    45: 34,
 };
-let d = c.apple * 4 + "I am a boy";
-console.log(d);
+function c() {
+    return "Hello, world!";
+}
+let func = () => {
+    return "Wow, this is also another string type";
+};
+//Creating classes in TypeScript
+class Car {
+    constructor(modelName, carYear) {
+        this.modelName = modelName;
+        this.carYear = carYear;
+    }
+    getMakeAndYear() {
+        return `The model name of the car is ${this.modelName} and the year is ${this.carYear}`;
+    }
+}
+let Toyota = new Car("Toyota Camry", 2021);
+console.log(Toyota.getMakeAndYear());
+let myAge = 745;
+let returnNum = () => {
+    return { age: myAge };
+};
 //# sourceMappingURL=index.js.map
--- typescript/Chapter02/src/index.ts
+++ typescript/Chapter02/src/index.ts
@@ -1,9 +1,44 @@
-let a = 1 + 2;
-let b = a + 3 + "Who are you";
-let c = {
-  apple: a,
-  banana: b,
-};
-let d = c.apple * 4 + "I am a boy";
+import { type } from "os";
 
-console.log(d);
+let a: {
+  b: number;
+  a: string;
+  c(): string;
+  [seatNumber: number]: number;
+} = {
+  a: "I am a boy",
+  b: 24,
+  c: () => `${a.b} and ${a.a}`,
+  23: 432,
+  45: 34,
+};
+
+function c(): string {
+  return "Hello, world!";
+}
+
+let func: () => string = () => {
+  return "Wow, this is also another string type";
+};
+//Creating classes in TypeScript
+class Car {
+  constructor(public modelName: string, public carYear: number) {}
+
+  getMakeAndYear(): string {
+    return `The model name of the car is ${this.modelName} and the year is ${this.carYear}`;
+  }
+}
+
+let Toyota: Car = new Car("Toyota Camry", 2021);
+
+console.log(Toyota.getMakeAndYear());
+
+//Type aliases
+type Age = number;
+type Name = string;
+
+let myAge: Age | Name = 745;
+
+let returnNum: () => { age: Age | Name } = () => {
+  return { age: myAge };
+};
